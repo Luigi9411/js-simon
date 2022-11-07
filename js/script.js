b42e4c8d6db1c2f3d70e3de680064ede10ab1a6c
Luigi9411 <luigi.sciavicco@gmail.com>
2022-11-07 18:41:32 +0100

    add-final-commit

--- js/script.js
+++ js/script.js
@@ -1,43 +1,76 @@
 
-// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-// Consigli del giorno:
-// * Pensate prima in italiano.
-// * Dividete in piccoli problemi la consegna.
-// * Individuate gli elementi di cui avete bisogno per realizzare il programma.
-// Attenzione che usando Google Chrome, il prompt può dare problemi con la visualizzazione dei numeri in pagina sui tempi di refresh del dom, lasciando i numeri visibili mentre il prompt è aperto.
+
+let nRandoms = 5;
+let minRandom = 1;
+let maxRandom = 100;
+let timeToHide = 30000;
 
 
+const eleNumbers = document.querySelector('.numbers');
+
+
+let arrRandoms = getArrRandomIntegers(nRandoms, minRandom, maxRandom);
+console.log(arrRandoms);
+
+for (let i = 0; i < arrRandoms.length; i++) {
+	eleNumbers.innerHTML += `<div class="number">${arrRandoms[i]}</div>`;
+}
+
+setTimeout(hideNumbers, timeToHide);
+setTimeout(finishGame, timeToHide + 100);
+
+function hideNumbers() {
+	eleNumbers.innerHTML = '';
+}
+
+function finishGame() {
+	const userNumbers = askNumbers();
+	console.log(userNumbers);
+	const guessedIndexes = verifyNumbers(arrRandoms, userNumbers);
+	console.log(guessedIndexes);
+	sayScore(arrRandoms, guessedIndexes);
+}
+
+function askNumbers() {
+	const inputUser = prompt('Dammi i numeri separati da spazio: '); // '51 23 1 5 15'
+	const arrInputUser = inputUser.split(' ');
+	for (let i = 0; i < arrInputUser.length; i++) {
+		arrInputUser[i] = parseInt(arrInputUser[i]);
+	}
+	return arrInputUser;
+}
+
+function verifyNumbers(arr1, arr2) {
+	const arrGuessedIndexes = [];
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] == arr2[i]) {
+			arrGuessedIndexes.push(i);
+		}
+	}
+	return arrGuessedIndexes;
+}
+
+function sayScore(arrValues, arrIndexes) {
+	let msg;
+	if (arrIndexes.length === 0) {
+		msg = 'Non ne hai azzeccato neanche uno!';
+	} else if (arrIndexes.length === arrValues.length) {
+		msg = 'Grande! Li hai azzeccati tutti!';
+	} else {
+		msg = `Ne hai azzeccati ${arrIndexes.length}, queste le posizioni azzeccate: ${arrIndexes.join(', ')}`;
+	}
+	console.log(msg);
+	alert(msg);
+}
+
+function getArrRandomIntegers(n, min, max) {
+	const arrRandoms = [];
+	for (let i = 0; i < n; i++) {
+		arrRandoms.push(getRandomInteger(min, max))
+	}
+	return arrRandoms;
+}
 
 function getRandomInteger(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
-function removeNumber(eleContainer){
-    eleNumbersRandom.classList.add('remove-numbers')
-}
-
-function spanNumber (eleNumberRandom) {
-    const text = parseInt(prompt('Inserisci il primo numero'));
-    const text2 = parseInt(prompt('Inserisci il secondo numero'));
-    const text3 = parseInt(prompt('Inserisci il terzo numero'));
-    const text4 = parseInt(prompt('Inserisci il quarto numero'));
-    const text5 = parseInt(prompt('Inserisci il quinto numero'))
-}
-
-const eleContainer = document.querySelector('.container');
-const eleNumbersRandom = document.querySelector('.numbers-random');
-let num = getRandomInteger(1,1000);
-let num1 = getRandomInteger(1,1000);
-let num2 = getRandomInteger(1,1000);
-let num3 = getRandomInteger(1,1000);
-let num4 = getRandomInteger(1,1000);
-
-setTimeout(removeNumber, 2000);
-eleNumbersRandom.innerHTML = (`${num} ${num1} ${num2} ${num3} ${num4}`)
-setTimeout(spanNumber, 3001);
-
-
-
-
